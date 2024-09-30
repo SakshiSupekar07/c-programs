7de5fab118abd496e5e0f4cb2abb703b24c323bb
SakshiSupekar07 <supekarsakshj07@gmail.com>
2024-09-30 09:45:59 +0530

    Add files via upload

---
+++ FILE/filewrite.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    FILE *fp;
+    fp=fopen("out.txt","w");
+    if(fp==NULL)
+    {
+        printf("Error opening file");
+
+    }
+    fprintf(fp,"Hello GOOD Morning!!!");
+    fclose(fp);
+    return 0;
+}
