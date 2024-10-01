3d3efcc7272c66cff032ee6aa21a9b0bc6cb0f35
SakshiSupekar07 <supekarsakshj07@gmail.com>
2024-10-01 09:54:46 +0530

    Update square.c

--- Functions/square.c
+++ Functions/square.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int squ(int x)
+{
+    int res=x*x;
+    return res;
+}
+int main()
+{
+    
+    int x=190;
+    int ans=squ(x);
+    printf("Answer is %d\n",ans);
+    return 0;
+}
