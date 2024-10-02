8f96bf883e1a194f190181013e9c9cf1620cf57a
SakshiSupekar07 <supekarsakshj07@gmail.com>
2024-10-02 11:51:59 +0530

    Add files via upload

---
+++ Functions/cubefunc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int cube(int x)
+{
+    int res=x*x*x;
+    return res;
+}
+int main()
+{
+    
+    int x=10;
+    int ans=cube(x);
+    printf("Answer is %d\n",ans);
+    return 0;
+}
