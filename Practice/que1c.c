ac7ba2b98ffbff77e5e4854f3d8172cf0c8c3993
SakshiSupekar07 <supekarsakshj07@gmail.com>
2024-10-11 10:27:45 +0530

    Add files via upload

---
+++ Practice/que1c.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    printf("Enter the two numbers");
+    scanf("%d%d",&i,&j);
+    printf("The value of i =%d\n The value of j = %d\n",i,j);
+    return 0;
+}
