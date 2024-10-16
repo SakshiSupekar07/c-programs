916e649bc62dd096c697147ff97292ac08b8dcb3
SakshiSupekar07 <supekarsakshj07@gmail.com>
2024-10-16 08:47:31 +0530

    Add files via upload

---
+++ Practice/que1g.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a, b,c;
+    printf("Enter the coefficient of a,b");
+    scanf("%d%d",&a,&b);
+    printf("Enter the constant");
+    scanf("%d",&c);
+    int res =(-b+sqrt(b*b-4*a*c))/(2*a);
+       printf("The root is%d\n",res);
+    int res1=(-b-sqrt(b*b-4*a*c))/(2*a);
+         printf("The root is%d\n",res1);
+    return 0; 
+
+}
