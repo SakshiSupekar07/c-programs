46eb612085e73de6386f740d8110185192a6c5f9
SakshiSupekar07 <supekarsakshj07@gmail.com>
2024-11-11 09:13:49 +0530

    Add files via upload

---
+++ Practice/practice.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    void *ptr =NULL;
+    printf("The value of pointer variable is %p\n",ptr);
+    int num=10;
+    ptr=&num;
+    printf("The address stored in the void pointer (ptr): %p\n", ptr);
+    printf("The value stored in the void pointer (ptr): %d\n", ptr);
+    printf("The value ofptr using typecasted void pointer: %d\n", *((int*)ptr));
+    double doubleVar = 3.14159;
+    ptr = &doubleVar;
+    printf("The value of ptr using typecasted void pointer: %f\n", *((double*)ptr));
+
+
+    return 0;
+}
