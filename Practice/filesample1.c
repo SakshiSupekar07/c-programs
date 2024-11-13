a3fd9b21627fd40da77789e21924d055ff3c5abc
SakshiSupekar07 <supekarsakshj07@gmail.com>
2024-11-13 08:40:56 +0530

    Add files via upload

---
+++ Practice/filesample1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    FILE *fp;
+  char ch;
+    fp=fopen("out.txt","r");
+    if(fp==NULL)
+    {
+        printf("Error opening file");
+
+    }
+    fscanf(fp,"%c",&ch);
+    printf("Character is=%c",ch);
+    fclose(fp);
+    return 0;
+}
