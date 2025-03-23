502637956c6a9ba287b25551856cba64644ac2ec
SakshiSupekar07 <supekarsakshj07@gmail.com>
2025-03-23 11:22:48 +0530

    Update practice.c

--- Practice/practice.c
+++ Practice/practice.c
@@ -6,7 +6,7 @@ int main()
     int num=10;
     ptr=&num;
     printf("The address stored in the void pointer (ptr): %p\n", ptr);
-    printf("The value stored in the void pointer (ptr): %d\n", ptr);
+    printf("The value stored in the void pointer (ptr): %d\n", ptr)
     printf("The value ofptr using typecasted void pointer: %d\n", *((int*)ptr));
     double doubleVar = 3.14159;
     ptr = &doubleVar;
